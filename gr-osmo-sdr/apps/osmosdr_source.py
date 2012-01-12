3dd0f54a5b4b6c3e7528ae4cf8a2ee4fad8f1fd6
Dimitri Stolnikov <horiz0n@gmx.net>
2012-01-12 22:24:08 +0100

    use fftsink in demo app

--- gr-osmo-sdr/apps/osmosdr_source.py
+++ gr-osmo-sdr/apps/osmosdr_source.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 ##################################################
 # Gnuradio Python Flow Graph
-# Title: Howto Square
+# Title: OsmoSDR Source
 # Generated: Thu Nov 12 11:26:07 2009
 ##################################################
 
@@ -10,7 +10,7 @@ from gnuradio import eng_notation
 from gnuradio import gr
 from gnuradio.eng_option import eng_option
 from gnuradio.gr import firdes
-from gnuradio.wxgui import scopesink2
+from gnuradio.wxgui import fftsink2
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
 import wx
@@ -18,56 +18,39 @@ import wx
 class osmosdr_source_c(grc_wxgui.top_block_gui):
 
 	def __init__(self):
-		grc_wxgui.top_block_gui.__init__(self, title="Howto Square")
+		grc_wxgui.top_block_gui.__init__(self, title="OsmoSDR Source")
 
 		##################################################
 		# Variables
 		##################################################
-		self.samp_rate = samp_rate = 10e3
+		self.samp_rate = samp_rate = 96e3
 
 		##################################################
 		# Blocks
 		##################################################
-		self.sink = scopesink2.scope_sink_f(
+		self.sink = fftsink2.fft_sink_c(
 			self.GetWin(),
-			title="Input",
+			fft_size=1024,
 			sample_rate=samp_rate,
-			v_scale=20,
-			v_offset=0,
-			t_scale=0.002,
-			ac_couple=False,
-			xy_mode=False,
-			num_inputs=1,
+			ref_scale=2.0,
+			ref_level=-30,
+			y_divs=10,
+			fft_rate=10,
+			average=True,
+			avg_alpha=0.5
 		)
+
 		self.Add(self.sink.win)
-		self.sink2 = scopesink2.scope_sink_f(
-			self.GetWin(),
-			title="Output",
-			sample_rate=samp_rate,
-			v_scale=0,
-			v_offset=0,
-			t_scale=0.002,
-			ac_couple=False,
-			xy_mode=False,
-			num_inputs=1,
-		)
-		self.Add(self.sink2.win)
-		self.sqr = osmosdr.source_c()
-		self.src = gr.vector_source_f(([float(n)-50 for n in range(100)]), True, 1)
-		self.thr = gr.throttle(gr.sizeof_float*1, samp_rate)
+		self.src = osmosdr.source_c()
 
 		##################################################
 		# Connections
 		##################################################
-		self.connect((self.thr, 0), (self.sqr, 0))
-		self.connect((self.src, 0), (self.thr, 0))
-		self.connect((self.thr, 0), (self.sink, 0))
-		self.connect((self.sqr, 0), (self.sink2, 0))
+		self.connect((self.src, 0), (self.sink, 0))
 
 	def set_samp_rate(self, samp_rate):
 		self.samp_rate = samp_rate
 		self.sink.set_sample_rate(self.samp_rate)
-		self.sink2.set_sample_rate(self.samp_rate)
 
 if __name__ == '__main__':
 	parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
